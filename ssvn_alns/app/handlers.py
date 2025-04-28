47bfe12d4abb80a606001abd4678d03663bfe3de
Фёдор Сорокин <f.m.sorokin@mail.ru>
2025-04-29 00:26:26 +0300

    Допилил второй день, необходим тест

--- ssvn_alns/app/handlers.py
+++ ssvn_alns/app/handlers.py
@@ -1,10 +1,11 @@
 from aiogram import Router, F, html
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 
 from . import keyboards as kb
+from aiogram.types import Message, CallbackQuery
 
 router = Router()
 
@@ -12,6 +13,11 @@ router = Router()
 class Registration(StatesGroup):
     waiting_for_name = State()
     waiting_for_birth_year = State()
+
+class SecondTest(StatesGroup):
+    fast_count_tester = State()
+    remembered_words = State()
+    stroop_test_time = State()
 
 
 # Обработчик команды start
@@ -48,7 +54,7 @@ async def get_birth_year(message: Message, state: FSMContext) -> None:
         return
 
 @router.callback_query(F.data == "first_day_test_call")
-async def first_day_test(query: F.CallbackQuery):
+async def first_day_test(query: CallbackQuery):
     # Первое сообщение
     await query.message.answer(
         text="Сегодня 1 день твоей тренировки, тебе предстоит выполнить 3 упражнения.\n"
@@ -60,5 +66,66 @@ async def first_day_test(query: F.CallbackQuery):
         text="1. На одной руке покажи детский жест примирения (сожми пальцы в кулак, а мизинец выпрями и слегка отклони).\n"
              "2. На другой руке покажи одобряющий жест — «класс» (сожми кулак, а затем подними большой палец вверх).\n"
              "Выполняй это упражнение поочередно правой и левой рукой.",
-        reply_markup=kb.complete
+        reply_markup=kb.complete  ## Пока добавляю везде комплиты, не хочу создавать много клавиатур, попробую подумать, как реализовать
     )
+
+## Обработка второго дня, сделал через FSM просто ради красивого вывода
+@router.callback_query(F.data == "second_day")
+async def second_day_test(query: CallbackQuery, state: FSMContext):
+    second_day_state = await state.get_state()
+    if second_day_state is None:
+        await query.message.answer(
+            text="Сегодня 2 день твоей тренировки, тебе предстоит выполнить 3 новых упражнения.\n"
+        )
+        await query.message.answer(
+            text="1. Тест на счет. Засеки время и максимально быстро посчитай вслух от 1 до 120."
+                 " Запиши свой результат (в секундах), например: 63"
+        )
+        await state.set_state(SecondTest.fast_count_tester)
+    else:
+        await query.message.answer(
+            text="Вы уже в процессе прохождения испытания."
+        )
+
+@router.message(SecondTest.fast_count_tester)
+async def get_fast_count_tester(message: Message, state: FSMContext):
+    await state.update_data(fast_count_tester=message.text)
+    await state.set_state(SecondTest.remembered_words)
+    await message.answer(
+        text="2. Тест на запоминание слов. В течение 2 минут постарайся запомнить как можно больше записанных ниже слов."
+             " Напиши здесь, в чат, слова, которые запомнил. Сколько слов ты смог вспомнить за 2 минуты?",
+    )
+
+@router.message(SecondTest.remembered_words)
+async def get_remembered_words(message: Message, state: FSMContext) -> None:
+    try: 
+        remembered_words_count = int(message.text)
+        await state.update_data(remembered_words=remembered_words_count)
+        await state.set_state(SecondTest.stroop_test_time)
+
+        await message.answer(
+            text="3. Тест Струпа. Называй вслух цвет слов, делая это как можно быстрее. Будь внимателен: ты должен не читать слова, а называть их цвет."
+                 " Если ошибешься, назови цвет еще раз. Отметь время (в секундах), которое тебе понадобилось. Например, 34"
+        )
+    except ValueError:
+        await message.answer(
+            text="Введите значение цифрой, например: 24"  
+        )
+        return
+
+@router.message(SecondTest.stroop_test_time)
+async def get_stroop_test_time(message: Message, state: FSMContext) -> None:
+    try:
+        stroop_time = int(message.text)
+        await state.update_data(stroop_test_time=stroop_time)
+
+        contained_data = await state.get_data()
+        await message.answer(
+            text=f"Ваши результаты:\n"
+                 f"1-й тест: {contained_data['fast_count_tester']} сек.\n"
+                 f"2-й тест: {contained_data['remembered_words']} слов\n"
+                 f"3-й тест: {contained_data['stroop_test_time']} сек."
+        )
+    except ValueError:
+        await message.answer("Введенное сообщение должно быть числом!")
+        return
--- ssvn_alns/app/keyboards.py
+++ ssvn_alns/app/keyboards.py
@@ -1,28 +1,30 @@
-from aiogram import types as T
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
 
 
 ## 3. Входное тестирование
-entry_test = T.InlineKeyboardMarkup(
+entry_test = InlineKeyboardMarkup(
     inline_keyboard=[
-        [T.InlineKeyboardButton(text="Первое тестирование", url="https://forms.gle/95pxWHj4rvwJzN318", callback_data="")], 
-        [T.InlineKeyboardButton(text="Второе тестирование", url="https://forms.gle/khJb1FowpBWKMujw9", callback_data="")],
-        [T.InlineKeyboardButton(text="Третье тестирование", url="https://forms.gle/HoepHvnXxNqAMHhX6", callback_data="")],
-        [T.InlineKeyboardButton(text="Четвертое тестирование", url="https://forms.gle/MW6TF3bZEeEB4ywa6", callback_data="")],
+        [InlineKeyboardButton(text="Первое тестирование", url="https://forms.gle/95pxWHj4rvwJzN318", callback_data="second_day")], 
+        [InlineKeyboardButton(text="Второе тестирование", url="https://forms.gle/khJb1FowpBWKMujw9", callback_data="second_day")],
+        [InlineKeyboardButton(text="Третье тестирование", url="https://forms.gle/HoepHvnXxNqAMHhX6", callback_data="second_day")],
+        [InlineKeyboardButton(text="Четвертое тестирование", url="https://forms.gle/MW6TF3bZEeEB4ywa6", callback_data="second_day")],
     ]
 )
 
 
-# ##4. Задание первого дня
-# first_day = T.InlineKeyboardButton(
-#     inline_keyboard=[
-#         [T.InlineKeyboardButton(text="Перейти к тестированию", callback_data="first_day_test_call")],
-#         [T.InlineKeyboardButton(text="Изучить теорию", callback_data="first_day_theory_call")]
-#     ]
-# )
+##4. Задание первого дня
+first_day = InlineKeyboardButton(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Перейти к тестированию", callback_data="first_day_test_call")],
+        [InlineKeyboardButton(text="Изучить теорию", callback_data="first_day_theory_call")]
+    ]
+)
 
-# #Клавиатура завершения действия
-# complete = T.InlineKeyboardMarkup(
-#     inline_keyboard=[
-#         [T.InlineKeyboardButton(text="Выполнено", callback_data="test_complete_call")]
-#     ]
-# )
+#Клавиатура завершения действия
+complete = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="Выполнено", callback_data="test_complete_call")]
+    ]
+)
