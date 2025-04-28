6e4f1441eb50070f0e9a1828e69b05f612b0ed31
py-tenz <f.m.sorokin@mail.ru>
2025-04-28 13:20:32 +0300

    keyboards.py

--- ssvn_alns/app/keyboards.py
+++ ssvn_alns/app/keyboards.py
@@ -1,1 +1,18 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+
+entry_test = InlineKeyboardMarkup[
+  inline_keyboard=[
+     [InlineKeyboardButton(text="Первое тестирование", url="https://forms.gle/95pxWHj4rvwJzN318")]
+     [InlineKeyboardButton(text="Второе тестирование", url="https://forms.gle/khJb1FowpBWKMujw9")]
+     [InlineKeyboardButton(text="Третье тестирование", url="https://forms.gle/HoepHvnXxNqAMHhX6")]
+     [InlineKeyboardButton(text="Четвертое тестирование", url="https://forms.gle/MW6TF3bZEeEB4ywa6")]
+  ]
+]
+
+first_day_test = InlineKeyboardMarkup[
+  inline_keyboard=[
+     [InlineKeyboardButton(text="Перейти к тренировке")]
+     [InlineKeyboardButton(text="Изучить теорию")]
+  ]
+]
+
