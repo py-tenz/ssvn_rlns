2620f22b03647de3c89db7420e066fbe2a9b60da
Фёдор Сорокин <f.m.sorokin@mail.ru>
2025-04-30 12:33:49 +0300

    Добавлен хэндлер на отработку коллбэка стартового тестирования

--- ssvn_alns/app/keyboards.py
+++ ssvn_alns/app/keyboards.py
@@ -6,16 +6,17 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 ## 3. Входное тестирование
 entry_test = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text="Первое тестирование", url="https://forms.gle/95pxWHj4rvwJzN318", callback_data="second_day")], 
-        [InlineKeyboardButton(text="Второе тестирование", url="https://forms.gle/khJb1FowpBWKMujw9", callback_data="second_day")],
-        [InlineKeyboardButton(text="Третье тестирование", url="https://forms.gle/HoepHvnXxNqAMHhX6", callback_data="second_day")],
-        [InlineKeyboardButton(text="Четвертое тестирование", url="https://forms.gle/MW6TF3bZEeEB4ywa6", callback_data="second_day")],
+        [InlineKeyboardButton(text="Первое тестирование", url="https://forms.gle/95pxWHj4rvwJzN318")], 
+        [InlineKeyboardButton(text="Второе тестирование", url="https://forms.gle/khJb1FowpBWKMujw9")],
+        [InlineKeyboardButton(text="Третье тестирование", url="https://forms.gle/HoepHvnXxNqAMHhX6")],
+        [InlineKeyboardButton(text="Четвертое тестирование", url="https://forms.gle/MW6TF3bZEeEB4ywa6")],
+        [InlineKeyboardButton(text="Выполнено", callback_data="entry_test_complete_call")],
     ]
 )
 
 
 ##4. Задание первого дня
-first_day = InlineKeyboardButton(
+first_day = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Перейти к тестированию", callback_data="first_day_test_call")],
         [InlineKeyboardButton(text="Изучить теорию", callback_data="first_day_theory_call")]
