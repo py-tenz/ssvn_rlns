18bc85f65bd15e085393b5e1ce9035282fedf944
Фёдор Сорокин <f.m.sorokin@mail.ru>
2025-05-01 22:20:45 +0300

    Добавлен новый файл dbconnection

--- ssvn_alns/app/dbconnection.py
+++ ssvn_alns/app/dbconnection.py
@@ -1,4 +1,11 @@
 import aiomysql
 import sqlalchemy as sa
+import asyncio
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
+loop = asyncio.get_event_loop()
+
+async def connection():
+    conn = await aiomysql.connect(host=127.0.01, port=3306, user='root', password=MYSQL_PASS, db="mysql")
+    
+    cursor = await conn.cursor
--- ssvn_alns/config.py
+++ ssvn_alns/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
+MYSQL_PASS = os.getenv("MYSQL_PASS")
 
 if not BOT_TOKEN:
     raise ValueError("BOT_TOKEN not found in environment variables")
